777d5c39f77d9afced0b569ba75cfc202c5e6472
Bryce Eadie <bryce.eadie@datadoghq.com>
2024-04-19 08:37:43 -0700

    Refactor with list comprehension

--- main.py
+++ main.py
@@ -16,10 +16,7 @@ while len(guessed_states) < 50:
     answer_state = screen.textinput(title=f"Correct guesses: {len(guessed_states)}/50", prompt="Enter the name of a state").title()
 
     if answer_state == "Exit":
-        un_guessed_states = []
-        for state in state_list:
-            if state not in guessed_states:
-                un_guessed_states.append(state)
+        un_guessed_states = [state for state in state_list if state not in guessed_states]
         new_data = pandas.DataFrame(un_guessed_states)
         new_data.to_csv("states_to_learn.csv")
         break
